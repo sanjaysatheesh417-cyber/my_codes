27ed8dfdfbd28653d5d8dd075fcf3942292500a7
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-08 23:06:05 -0700

    new file

--- hackerrank/testcases/test_algorithm/test_utopian_tree.py
+++ hackerrank/testcases/test_algorithm/test_utopian_tree.py
@@ -2,8 +2,8 @@ import pytest
 
 from hackerrank.solutions.algorithms.utopian_tree import utopianTree
 
-@pytest.mark.parametrize("n,expected",[(0,1),(1,2),(4,7),(5,14)],
-ids=["TESTCASE1","TESTCASE2","TESTCASE3","TESTCASE4"])
+@pytest.mark.parametrize("n,expected",[(0,1),(1,2),(4,7),(5,14)]
+, ids=["TESTCASE1","TESTCASE2","TESTCASE3","TESTCASE4"])
 
-def test_utopiantree(n,expected):
+def test_utopianTree(n, expected):
     assert utopianTree(n) == expected
