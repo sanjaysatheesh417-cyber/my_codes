8aefba7b1082b14d2bc2d6dc2421f0d587dfced8
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-08 23:38:02 -0700

    modified

--- hackerrank/testcases/test_algorithm/test_bill_division.py
+++ hackerrank/testcases/test_algorithm/test_bill_division.py
@@ -1,6 +1,6 @@
 import pytest
 
-from hackerrank.solutions.algorithms.bil_division import bonAppetit
+from hackerrank.solutions.algorithms.bill_division import bonAppetit
 
 @pytest.mark.parametrize("bill,k,b,expected",[([3,10,2,9],1,12,5),([3,10,2,9],1,7,"Bon Appetit")]
 , ids=["TESTCASE1","TESTCASE2"])
