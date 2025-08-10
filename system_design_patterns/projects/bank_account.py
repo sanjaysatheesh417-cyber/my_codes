8494b236ec3833ff65ad134d0f94c90b63538e17
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-09 18:05:34 -0700

    modified

--- system_design_patterns/projects/bank_account.py
+++ system_design_patterns/projects/bank_account.py
@@ -1,14 +1,18 @@
-class bank_account:
+class BankAccount:
     def __init__(self, name, balance, age, bank):
         self.name = name
         self.balance = balance
         self.age = age
         self.bank = bank
-    def withdraw(self, amount):
+        print(f"congratulations {self.name}, you have successfully created your account ")
+    def withdraw(self, amount):#for withdrawing cash
         self.balance -= amount
         print(f"amount {amount} has been withdrawn and {self.balance} is your balance")
-    def deposit(self, amount):
+    def deposit(self, amount):#for depositing
         self.balance += amount
         print(f"amount {amount} has been deposited and {self.balance} is your balance")
-    def check_balance(self):
+    def check_balance(self):#to check balance
         print(f"Your balance is {self.balance}")
+    def transfer(self, amount, to):#to transfer money to another bank
+        self.balance -= amount
+        print(f"amount {amount} has been transferred to {to} and {self.balance} is your balance")
