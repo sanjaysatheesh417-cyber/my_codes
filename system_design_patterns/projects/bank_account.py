cc39f988efd092ef554d083da5c95915fe17f746
sanjaysatheesh <sanjaysatheesh417@gmail.com>
2025-08-10 22:48:27 -0700

    modified

--- system_design_patterns/projects/bank_account.py
+++ system_design_patterns/projects/bank_account.py
@@ -1,13 +1,39 @@
-class BankAccount:
-    def __init__(self, name, balance, age, bank):
+class Account:
+    def __init__(self, name, balance, age, bank ,savings):
         self.name = name
         self.balance = balance
         self.age = age
         self.bank = bank
-        print(f"congratulations {self.name}, you have successfully created your account ")
+        self.savings = savings
+class SavingsAccount(Account): #subclass for savings account
+    def __init__(self, name, balance, age, bank ,savings):
+        super().__init__(name,balance,age,bank,savings)
+        print(f"congratulations {self.name}, you have successfully created your savings account ")
+    def save(self, amount):
+        if self.balance >= amount:
+            self.savings += amount
+            self.balance -= amount
+            print(f"savings amount {amount}, you have successfully saved {amount}")
+        else:
+            print("insufficient balance")
+    def transfer_to_checking_account(self, amount):#to transfer your savings
+        if self.savings >= amount:
+            self.balance += amount
+            print(f"You have successfully transferred {amount} to checking account")
+        else:
+            print("insufficient balance")
+    def check_savings(self):#to check savings account balance
+        print(f"Your savings is {self.savings}")
+class CheckingAccount(Account):#subclass for checking account
+    def __init__(self, name, balance, age, bank, savings):
+        super().__init__(name,balance,age,bank ,savings)
+        print(f"congratulations {self.name}, you have successfully created your checking account ")
     def withdraw(self, amount):#for withdrawing cash
-        self.balance -= amount
-        print(f"amount {amount} has been withdrawn and {self.balance} is your balance")
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"amount {amount} has been withdrawn and {self.balance} is your balance")
+        else:
+            print("insufficient balance")
     def deposit(self, amount):#for depositing
         self.balance += amount
         print(f"amount {amount} has been deposited and {self.balance} is your balance")
--- system_design_patterns/test_projects/test_bank_account.py
+++
@@ -1,8 +0,0 @@
-
-from system_design_patterns.projects.bank_account import BankAccount
-
-acc=BankAccount("sanjay",10000,18,"Axis")
-acc.withdraw(1000)
-acc.check_balance()
-acc.deposit(5000)
-acc.transfer(5000,"IDBI")
