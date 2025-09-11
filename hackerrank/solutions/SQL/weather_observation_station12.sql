d5d50a37f367645ef12dd2496cd1df329a24d7a5
SAN_CODES <sanjaysatheesh417@gmail.com>
2025-09-11 08:51:05 -0700

    Create weather_observation_station12.sql
    new file

---
+++ hackerrank/solutions/SQL/weather_observation_station12.sql
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true
+
+solution:
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT (CITY LIKE "A%" OR CITY LIKE "E%" OR CITY LIKE "I%" OR CITY LIKE "O%" OR CITY LIKE "U%")
+AND NOT (CITY LIKE "%A" OR CITY LIKE "%E" OR CITY LIKE "%I" OR CITY LIKE "%O" OR CITY LIKE "%U");
