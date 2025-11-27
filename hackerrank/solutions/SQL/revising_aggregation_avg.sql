9a36e8aea8fe506c6c3f46041603bc3268138a7e
SAN_CODES <sanjaysatheesh417@gmail.com>
2025-11-27 23:25:58 +0530

    Add SQL query to calculate average city population
    Removed commented URL and added SQL solution to calculate average population.

---
+++ hackerrank/solutions/SQL/revising_aggregation_avg.sql
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+
+solution:
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = "California";
