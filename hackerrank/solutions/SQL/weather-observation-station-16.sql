b230d5304445f38ca51c07ddfbffec9c35cb0228
SAN_CODES <sanjaysatheesh417@gmail.com>
2026-02-22 20:49:03 +0530

    Implement SQL query for weather observation station 16
    Add SQL solution for weather observation station challenge.

---
+++ hackerrank/solutions/SQL/weather-observation-station-16.sql
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/weather-observation-station-16/problem
+
+solution:
+SELECT ROUND(LAT_N,4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);
