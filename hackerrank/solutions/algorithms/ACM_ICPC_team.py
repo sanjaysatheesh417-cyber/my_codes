81cd5b31d8193e2646f3a5a395cd0f21f0b0e94e
sanjay <helium000.bluesky@gmail.com>
2026-02-22 20:06:19 +0530

    modify

--- hackerrank/solutions/algorithms/ACM_ICPC_team.py
+++ hackerrank/solutions/algorithms/ACM_ICPC_team.py
@@ -9,17 +9,13 @@ def acmTeam(topic):
     counts = []
     pairs = []
     for i in range(len(topic)):
-        for j in range(len(topic)):
-            if {i,j} not in pairs:
-                pairs.append({i,j})
-    pairs1 = map(list(lambda x: list(x),pairs))
-    for m in pairs1:
+        for j in range( i+1,len(topic)):
+            pairs.append((topic[i], topic[j]))
+    for pair in pairs:
         count = 0
-        for l in range(len(topic[0])):
-            if topic[m[0]][l]==0 and topic[m[1]][l]==0:
-                count += 0
-            else:
+        for l in range(len(pair[0])):
+            if (pair[0][l] == "1") or (pair[1][l] == "1"):
                 count += 1
         counts.append(count)
-    return pairs
-acmTeam(["10101","11100","11010","00101"])
+    return (max(counts),counts.count(max(counts)))
+acmTeam(["10101","11110","00010"])
