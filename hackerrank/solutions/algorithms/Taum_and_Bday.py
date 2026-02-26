fcbde3c7f70e7596b01e8d911c05ba18627a255e
sanjay <helium000.bluesky@gmail.com>
2026-02-26 22:29:13 +0530

    new solution

--- hackerrank/solutions/algorithms/Taum_and_Bday.py
+++ hackerrank/solutions/algorithms/Taum_and_Bday.py
@@ -7,4 +7,19 @@ import sys
 def taumBday(b, w, bc, wc, z):
     # Write your code here
     min_cost = 0
-    if b*bc <
+    if b*bc > (wc+z)*b:
+        if w*wc > (bc+z)*w:
+            min_cost += (wc+z)*b
+            min_cost += (bc+z)*w
+        else:
+            min_cost += (wc+z)*b
+            min_cost += w*wc
+    else:
+        if w * wc < (bc + z) * w:
+            min_cost += w*wc
+            min_cost += b*bc
+        else:
+            min_cost += (bc + z) * w
+            min_cost += b*bc
+    return min_cost
+taumBday(3,6,9,1,1)
