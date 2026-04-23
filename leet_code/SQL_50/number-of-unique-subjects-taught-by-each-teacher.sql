4400c220f1754071d8b40a10cc97765500d2d75a
SAN_CODES <sanjaysatheesh417@gmail.com>
2026-04-23 18:26:14 +0530

    Create SQL query for unique subjects by teacher
    Add SQL query to count unique subjects taught by each teacher.

---
+++ leet_code/SQL_50/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/?envType=study-plan-v2&envId=top-sql-50
+
+solution:
+SELECT
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id;
