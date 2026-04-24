481b774560a3b32285d11af0271d442d0ac1e829
SAN_CODES <sanjaysatheesh417@gmail.com>
2026-04-24 19:43:01 +0530

    Create SQL query for classes with minimum 5 students
    Add SQL query to find classes with at least 5 students.

---
+++ leet_code/SQL_50/classes-with-at-least-5-students.sql
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/classes-with-at-least-5-students/?envType=study-plan-v2&envId=top-sql-50
+
+solution:
+SELECT
+    class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
