5657a77c80e724f8441dadf35e15986298cde334
SAN_CODES <sanjaysatheesh417@gmail.com>
2026-04-24 19:51:39 +0530

    Implement followers count query
    Add SQL query to count followers for each user.

---
+++ leet_code/SQL_50/find-followers-count.sql
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50
+
+solution:
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id;
